44fdf5cd024ce258580f1590f139b408eed8ed2a
aryans1319 <aryan4p130202@gmail.com>
2026-03-15 14:53:52 +0530

    feat: final mvp setup

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/aryans1319/devdoctor/cmd"
+import (
+	"os"
+
+	"github.com/aryans1319/devdoctor/cmd"
+)
 
 func main() {
+	// If first argument is "serve", start the web server
+	// Otherwise run the CLI
+	if len(os.Args) > 1 && os.Args[1] == "serve" {
+		startServer()
+		return
+	}
+
 	cmd.Execute()
 }
